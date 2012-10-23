587cd262efea1459734f65cbe8c764a9830ca5cd
Alfredo Altamirano <fredy.altamirano8@gmail.com>
2012-10-22 19:09:47 -0500

    added texture grabbing with mouse

--- src/mapeditor/main/Tools.java
+++ src/mapeditor/main/Tools.java
@@ -9,14 +9,16 @@ import java.util.List;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.util.Point;
 import org.newdawn.slick.opengl.Texture;
+import static org.lwjgl.opengl.GL11.*;
 
 public class Tools {
+	
+	//TODO read object's size and render as that size when grabbed
 
-	public static final int WIDTH = 200, TAB_WIDTH = 25, TAB_HEIGHT = 90;
-	
+	public static final int WIDTH = 200, TAB_WIDTH = 25, TAB_HEIGHT = 90, COLUMNS = 4;
 	public static List<Texture> textures = new ArrayList<Texture>();
-	
 	private static int selectedTab = 0;
+	private static Texture grabbedTexture;
 	
 	static
 	{
@@ -44,19 +46,25 @@ public class Tools {
 				Util.write(tabName, Main.MAP_SIZE.getWidth()-Util.getTextWidth(tabName)/2-13, 30 + 93*i + Util.getFontHeight()*j);
 			}
 			
+			//render the entity textures
 			if(selectedTab == i)
 			{
-				int columns = 4;
 				for(int j=0; j<Tabs.values()[i].getTextures().size() ; j++)
 				{
 					Texture texture = Tabs.values()[i].getTextures().get(j);
-					Util.render(texture, Main.MAP_SIZE.getWidth() + j%columns*35 + 25, j/columns*35 + 70, 32, 32, texture.getTextureWidth(), texture.getTextureHeight());
+					Util.render(texture, Main.MAP_SIZE.getWidth() + j%COLUMNS*35 + 25, j/COLUMNS*35 + 70, 32, 32, texture.getTextureWidth(), texture.getTextureHeight());
 				}
 			}
 		}
 		
 		Util.write(Tabs.values()[selectedTab].name(), Main.MAP_SIZE.getWidth() + 70, 20); //TODO center aligned with Util.getTextWidth
-				
+		
+		if(grabbedTexture != null)
+		{
+			glColor4f(1, 1, 1, .5f);
+			Util.render(grabbedTexture, Mouse.getX(), Main.MAP_SIZE.getHeight() - Mouse.getY() + 1, 32, 32, grabbedTexture.getTextureWidth(), grabbedTexture.getTextureHeight());
+			glColor4f(1, 1, 1, 1f);
+		}
 	}
 	
 	public static void mouse()
@@ -65,12 +73,14 @@ public class Tools {
 		{
 			if(!Mouse.getEventButtonState() && Mouse.getDX() == 0 && Mouse.getDY() == 0) //on mouse release
 			{
+				
+				int x = Mouse.getX();
+				int y = Main.MAP_SIZE.getHeight() - Mouse.getY() + 1;
+				
+				//Checks for tab click
 				for(int i=0; i<Tabs.values().length; i++)
 				{
 					Point tabPos = new Point(Main.MAP_SIZE.getWidth()-TAB_WIDTH, 30+92*i);
-					
-					int x = Mouse.getX();
-					int y = Main.MAP_SIZE.getHeight() - Mouse.getY() + 1;
 					
 					if(x >= tabPos.getX() && x <= tabPos.getX() + TAB_WIDTH &&
 					   y >= tabPos.getY() && y <= tabPos.getY() + TAB_HEIGHT) //inside ith tab
@@ -78,6 +88,19 @@ public class Tools {
 						selectedTab = i;
 					}
 					
+				}
+				
+				//check for texture click
+				for(int i=0; i<Tabs.values()[selectedTab].getTextures().size(); i++)
+				{
+					
+					Point texturePos = new Point(Main.MAP_SIZE.getWidth() + i%COLUMNS*35 + 25, i/COLUMNS*35 + 70);
+					
+					if(x >= texturePos.getX() && x <= texturePos.getX() + 32 &&
+					   y >= texturePos.getY() && y <= texturePos.getY() + 32) //click on the texture
+					{
+						grabbedTexture = Tabs.values()[selectedTab].getTextures().get(i);
+					}
 				}
 			}
 		}
